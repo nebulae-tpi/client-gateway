9da56c70380a6ef469b020f99c610369f05a7eb8
Jaime Zapata <esteban.zapata@nebulae.com.co>
2019-04-02 16:30:50 -0500

    Updated predefined message query

--- api/client-gateway/graphql/tmp-params/resolvers.js
+++ api/client-gateway/graphql/tmp-params/resolvers.js
@@ -10,29 +10,38 @@ const buildGoogleMapsParams = () => {
 }
 
 const buildPredefinedMessages = () => {
-  return process.env.PREDEFINED_MESSAGES.messages;
+  return process.env.PREDEFINED_MESSAGES_CLIENT.messages;
 }
+
 
 
 module.exports = {
 
   Query: {
 
-    GoogleMapsParams: (root, args, context, info) => {
-      return of({
-        //googleMapsAndroidKey: process.env.GOOGLE_MAPS_ANDROID_KEY,
-        googleMapsBrowserKey: ''
-      }).toPromise()
+    // GoogleMapsParams: (root, args, context, info) => {
+    //   return of({
+    //     //googleMapsAndroidKey: process.env.GOOGLE_MAPS_ANDROID_KEY,
+    //     googleMapsBrowserKey: ''
+    //   }).toPromise()
+    // },
+
+    // Params: (root, args, context, info) => {
+    //   return of({
+    //     GoogleMapsParams: buildGoogleMapsParams(),
+    //     predefinedMessages: buildPredefinedMessages()
+    //   }).toPromise()
+    // },
+
+    PredefinedMessages: (root, args, context, info) => {
+      return of(buildPredefinedMessagesClient())
+      .toPromise()
     },
 
-
-    Params: (root, args, context, info) => {
-      return of({
-        GoogleMapsParams: buildGoogleMapsParams(),
-        predefinedMessages: buildPredefinedMessages()
-      }).toPromise()
-    },
-
+    // DriverPredefinedMessages: (root, args, context, info) => {
+    //   return of(buildPredefinedMessagesDriver())
+    //   .toPromise()
+    // },
   },
 }
 
