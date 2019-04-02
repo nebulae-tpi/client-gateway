51dc903e4444076cc0c59b3bdb9f139da124ae8d
Jaime Zapata <esteban.zapata@nebulae.com.co>
2019-04-02 16:50:44 -0500

    Updated predefined message query

--- deployment/docker/build-docker-hub.sh
+++ deployment/docker/build-docker-hub.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export DOCKERHUB_IMAGE=clientgateway
-export DOCKERHUB_TAG=0.0.7
+export DOCKERHUB_TAG=0.0.8
 
 rm -rf deployment/docker/client-gateway/
 cp -R $API_SHELL_PATH/client-gateway deployment/docker/client-gateway
