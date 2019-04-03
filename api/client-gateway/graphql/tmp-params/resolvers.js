400e2885eabd6b9fb31532092b653b9fe6f23dab
Jaime Zapata <esteban.zapata@nebulae.com.co>
2019-04-03 08:57:47 -0500

    Fixed some errors

--- api/client-gateway/graphql/tmp-params/resolvers.js
+++ api/client-gateway/graphql/tmp-params/resolvers.js
@@ -1,40 +1,18 @@
-const { of, Observable, bindNodeCallback } = require('rxjs');
-const { map, tap } = require('rxjs/operators');
+const { from, of, Observable, bindNodeCallback } = require('rxjs');
+const { map, tap, toArray } = require('rxjs/operators');
 const request = require('request');
 
-const buildGoogleMapsParams = () => {
-  return {
-    googleMapsAndroidKey: process.env.GOOGLE_MAPS_ANDROID_KEY,
-    googleMapsBrowserKey: process.env.GOOGLE_MAPS_BROWSER_KEY
-  };
+const buildPredefinedMessages = () => {  
+  const predefinedMessages = JSON.parse(process.env.PREDEFINED_MESSAGES_CLIENT);
+  return predefinedMessages.messages;
 }
-
-const buildPredefinedMessages = () => {
-  return process.env.PREDEFINED_MESSAGES_CLIENT.messages;
-}
-
-
 
 module.exports = {
 
   Query: {
 
-    // GoogleMapsParams: (root, args, context, info) => {
-    //   return of({
-    //     //googleMapsAndroidKey: process.env.GOOGLE_MAPS_ANDROID_KEY,
-    //     googleMapsBrowserKey: ''
-    //   }).toPromise()
-    // },
-
-    // Params: (root, args, context, info) => {
-    //   return of({
-    //     GoogleMapsParams: buildGoogleMapsParams(),
-    //     predefinedMessages: buildPredefinedMessages()
-    //   }).toPromise()
-    // },
-
     PredefinedMessages: (root, args, context, info) => {
-      return of(buildPredefinedMessagesClient())
+      return of(buildPredefinedMessages())
       .toPromise()
     }
   },
