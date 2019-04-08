ac1fdee9f449adc34410702e1bda770adc6e73d1
Jaime Zapata <esteban.zapata@nebulae.com.co>
2019-04-08 14:42:58 -0500

    Added keep alive param to apollo server

--- api/client-gateway/server.js
+++ api/client-gateway/server.js
@@ -105,7 +105,7 @@ const server = new ApolloServer({
     },
     subscriptions: {
         path: process.env.GRAPHQL_WS_END_POINT,
-        keepAlive: 20,
+        keepAlive: 1,
         onConnect: async (connectionParams, webSocket, connectionContext) => {           
             console.log(`GraphQL_WS.onConnect: origin=${connectionContext.request.headers.origin} url=${connectionContext.request.url}`);
             const encondedToken$ = connectionParams.authToken
