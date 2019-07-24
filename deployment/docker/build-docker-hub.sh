6f1b2d93084ecd1f0f9b507cff922a7b2b4e156a
juansantane <juan.santa@nebulae.com.co>
2019-07-24 17:16:53 -0500

    query handler created to get price per kilometer on trip usin peak hour ranges

--- deployment/docker/build-docker-hub.sh
+++ deployment/docker/build-docker-hub.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export DOCKERHUB_IMAGE=clientgateway
-export DOCKERHUB_TAG=0.0.17
+export DOCKERHUB_TAG=0.0.18
 
 rm -rf deployment/docker/client-gateway/
 cp -R $API_SHELL_PATH/client-gateway deployment/docker/client-gateway
