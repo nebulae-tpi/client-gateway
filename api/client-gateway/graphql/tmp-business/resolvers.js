4675913f9a45f443f22fb988e98037538cf1b345
juansantane <juan.santa@nebulae.com.co>
2019-09-24 13:35:55 -0500

    logs were added to see the businessid in authToken when BusinessContactInfo query is used

--- api/client-gateway/graphql/tmp-business/resolvers.js
+++ api/client-gateway/graphql/tmp-business/resolvers.js
@@ -5,6 +5,26 @@ const request = require('request');
 module.exports = {
   Query: {
     BusinessContactInfo: (root, args, context, info) => {
+
+      const businessInits = [
+        {
+          name: 'NebulaE',
+          whatsapp: 573004832728,
+          phone: 3004832728,
+          businessId: '423f-54a3-434a-645b'
+        },
+        {
+          name: 'TxPlus Manizales',
+          whatsapp: 573004832728,
+          phone: 3004832728,
+          businessId: 'b19c067e-57b4-468f-b970-d0101a31cacb'
+        }
+      ];
+      const { businessId } = context.authToken;
+      console.log('[QUERY]BusinessContactInfo; businessId del token  ==> ', businessId );
+      const businessUnit = businessInits.find(bu => bu.businessId === businessId);
+      console.log('Unidad encontrada ==> ', businessUnit);
+      
       return of(
         {
           name: 'NebulaE',
